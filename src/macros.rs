d66fb710d615791df82ea4ae176fb3a3d9e62eee
Keating Reid <keating.reid@pm.me>
2023-09-09 06:58:17 -0400

    Annotating test macro with #[cfg(test)]

--- src/macros.rs
+++ src/macros.rs
@@ -3,7 +3,7 @@ macro_rules! oserr {
         std::io::Error::last_os_error()
     };
 }
-#[allow(unused)]
+#[cfg(test)]
 macro_rules! assert_ok {
     ($val:ident) => {
         assert!($val.is_ok(), "{:?}", $val.unwrap_err())
